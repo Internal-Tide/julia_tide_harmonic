25a88d3b987c1cdde346c7e0a240a0711deb3c1c
Zhiwei <tianzhiwei@stu.ouc.edu.cn>
2023-03-01 23:08:43 +0800

    create the repo

---
+++ src/t_getconsts.jl
@@ -0,0 +1,95 @@
+using NCDatasets
+
+include("t_astron.jl")
+
+
+# set the base directory for the data
+_base_dir = joinpath(dirname(@__FILE__), "..", "data")
+const has_const = isfile(joinpath(_base_dir, "t_constituents_const.nc"))
+const has_sat = isfile(joinpath(_base_dir, "t_constituents_sat.nc"))
+const has_shallow = isfile(joinpath(_base_dir, "t_constituents_shallow.nc"))
+if (has_const & has_sat & has_shallow)
+    _const = Dict()
+    _sat = Dict()
+    _shallow = Dict()
+
+    constituents_const = Dataset(joinpath(_base_dir, "t_constituents_const.nc"))
+    for key in keys(constituents_const)
+        _const[key] = constituents_const[key][:]
+    end
+
+    constituents_sat = Dataset(joinpath(_base_dir, "t_constituents_sat.nc"))
+    for key in keys(constituents_sat)
+        _sat[key] = constituents_sat[key][:]
+    end
+
+    constituents_shallow = Dataset(joinpath(_base_dir, "t_constituents_shallow.nc"))
+    for key in keys(constituents_shallow)
+        _shallow[key] = constituents_shallow[key][:]
+    end
+
+    close(constituents_const)
+    close(constituents_sat)
+    close(constituents_shallow)
+
+else
+    println("You do not have t_constituents_*.nc "$
+    "check that package installation is correct.")
+    _const = Dict()
+    _sat = Dict()
+    _shallow = Dict()
+end
+
+
+
+function t_getconsts(ctime)
+    """
+    t_getconsts - Gets constituent data structures holding
+                  information for tidal analyses
+    Variables are loaded from 't_constituents_*.npy'
+    on init and a copy is made now.
+    When ctime is specified t_getconsts recomputes the frequencies from
+    the rates-of-change of astronomical parameters at the matlab TIME given.
+
+     :Parameters:
+        ctime: a datetime, the start time of the data input into t_tide.
+
+    Note:
+        Not sure if a copy has to be made here or if they can be used directly.
+        For now a copy should be much fast then a load.
+    """
+    consts = deepcopy(_const)
+    sat = deepcopy(_sat)
+    shallow = deepcopy(_shallow)
+
+    if ctime !== nothing
+        # If no time, just take the "standard" frequencies, otherwise
+        # compute them from derivatives of astro parameters. This is
+        # probably a real overkill - the diffs are in the
+        # 10th decimal place (9th sig fig).
+        astro, ader = t_astron(ctime)
+
+        ii = .!isnan.(consts["ishallow"])
+        println(size(ii))
+        println(size(consts["doodson"]))
+        println(size(ader))
+        println(size(consts["freq"]))
+        consts["freq"][.!ii] .= (consts["doodson"][.!ii, :] * ader) / 24
+
+        shallow_m1 = consts["ishallow"] .- 1
+        iname_m1 = shallow["iname"] .- 1
+        range_cache = Dict(n => collect(0:n-1) for n in 0:maximum(consts["nshallow"]))
+        for k in findall(ii)
+            ik = shallow_m1[k] .+ range_cache[consts["nshallow"][k]]
+            consts["freq"][k] .= dot(consts["freq"][iname_m1[ik]], shallow["coef"][ik])
+        end
+    end
+
+    return consts, sat, shallow
+end
+
+# _const["ishallow"]
+
+ii = .!isnan.(_const["ishallow"])
+# _const["freq"]
+_const["doodson"]
---
+++ src/t_vuf.jl
@@ -0,0 +1,7 @@
+using Dates
+
+include("t_getconsts.jl")
+
+ctime = DateTime(2015, 1, 1)
+
+t_getconsts(ctime)
